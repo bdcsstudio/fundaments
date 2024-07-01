d03d54d2930e60290554b7886c772c1c51492912
bdcsstudio <info@bodacious.studio>
2024-07-01 14:33:13 +0200

    Navtrigger en live search commit

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 import "./src/split-heading/split-heading";
+
 import "./src/nav/nav-search-expand";
+import "./src/nav/nav-search";
+import "./src/nav/navtrigger";
 
 import "./src/video/preview-hover";
 import "./src/video/plyr";
