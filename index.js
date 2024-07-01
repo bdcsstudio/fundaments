e519113869877145f15d0c6be76a9aa8e320234b
bdcsstudio <info@bodacious.studio>
2024-07-01 10:24:14 +0200

    Video Player Commit

--- index.js
+++ index.js
@@ -2,3 +2,6 @@ import "./src//nav/global-search";
 import "./src//nav/global-search-expand";
 import "./src//nav/active-menus";
 import "./src//split-heading/split-heading";
+import "./src//horizontal-scrub";
+import "./src//video/plyr";
+import "./src//video/hover-ix";
